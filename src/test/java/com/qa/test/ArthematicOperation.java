0baa983878997bc343557da1a401d9c32dbac37c
Varun Pandey <varun.ece@gmail.com>
2018-09-19 20:17:35 +0530

    test2

---
+++ src/test/java/com/qa/test/ArthematicOperation.java
@@ -0,0 +1,19 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+public class ArthematicOperation {
+	
+	@Test
+	public void add()
+	{
+		System.out.println("Hello");
+	}
+	
+	@Test
+	public void subs()
+	{
+		System.out.println("Hi");
+	}
+
+}
